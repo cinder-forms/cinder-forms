3052a24d2087e7e9c3fe2dd9444a5ce51f7984df
Niklas Portmann <niklaspor@gmail.com>
2020-03-27 20:08:34 +0100

    generate libraries

---
+++ projects/cinder-forms/validators/src/lib/validators.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-Validators',
+  template: `
+    <p>
+      validators works!
+    </p>
+  `,
+  styles: []
+})
+export class ValidatorsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
