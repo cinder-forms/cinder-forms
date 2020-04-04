0a53f32ec3772ecead1f08f8f0419cab24825fec
Niklas <niklaspor@gmail.com>
2020-04-04 20:01:09 +0200

    add subscription boilerplate

--- projects/cinder-forms/core/src/lib/directives/control/cinder-control.directive.ts
+++ projects/cinder-forms/core/src/lib/directives/control/cinder-control.directive.ts
@@ -1,9 +1,10 @@
 import { Directive, Input } from '@angular/core';
 import { BehaviorSubject, NEVER } from 'rxjs';
-import { exhaustMap } from 'rxjs/operators';
+import { exhaustMap, map, distinctUntilChanged } from 'rxjs/operators';
 import { CinderControlState } from '../../logic/control/init/types';
 import { toUpdate } from '../../logic/utils/types';
 import { connectControl } from './../../connector/control/connect';
+import { circularDeepEqual } from 'fast-equals';
 
 type TControlState = CinderControlState<{}, any>;
 type TControlUpdate = toUpdate<TControlState>;
@@ -20,7 +21,7 @@ export class CinderControlDirective {
   private readonly control$ = this.connector$.pipe(exhaustMap((connector) => connector.control$));
 
   constructor() {
-    this.control$.subscribe(console.log);
+    this.subscribeToControls();
   }
 
   @Input('cinderControl')
@@ -30,5 +31,52 @@ export class CinderControlDirective {
 
   private update(update: TControlUpdate) {
     this.connector$.value.update(update);
+  }
+
+  private setDirty(dirty: boolean) {}
+
+  private setTouched(touched: boolean) {}
+
+  private setInvalid(invalid: boolean) {}
+
+  private setChanged(changed: boolean) {}
+
+  private setValue(value: any) {}
+
+  private subscribeToControls() {
+    this.control$
+      .pipe(
+        map((control) => control.dirty),
+        distinctUntilChanged()
+      )
+      .subscribe((dirty) => this.setDirty(dirty));
+
+    this.control$
+      .pipe(
+        map((control) => control.touched),
+        distinctUntilChanged()
+      )
+      .subscribe((touched) => this.setTouched(touched));
+
+    this.control$
+      .pipe(
+        map((control) => control.invalid),
+        distinctUntilChanged()
+      )
+      .subscribe((invalid) => this.setInvalid(invalid));
+
+    this.control$
+      .pipe(
+        map((control) => control.changed),
+        distinctUntilChanged()
+      )
+      .subscribe((changed) => this.setChanged(changed));
+
+    this.control$
+      .pipe(
+        map((control) => control.value),
+        distinctUntilChanged((val1, val2) => circularDeepEqual(val1, val2))
+      )
+      .subscribe((value) => this.setValue(value));
   }
 }
